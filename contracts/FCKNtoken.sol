9ee0bfc6fe888e483588e752874b74fdd42b92ae
Michael Thu <mikemthu@gmail.com>
2021-07-27 23:12:14 +0900

    update to deploy to BSC testnet

--- contracts/FCKNtoken.sol
+++ contracts/FCKNtoken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FCKN is ERC20 {
     constructor(uint256 initialSupply) ERC20("FCKN Fried Chicken", "FCKN") {
-        _mint(msg.sender, initialSupply * 10 * decimals());
+        _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 }
